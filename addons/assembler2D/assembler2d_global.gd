69c32d2a69a319d06fcb0db55fb949c592bd22de
jarlowrey <jarlowrey@gmail.com>
2017-09-20 11:50:08 -0400

    better rotation

--- addons/assembler2D/assembler2d_global.gd
+++ addons/assembler2D/assembler2d_global.gd
@@ -16,18 +16,15 @@ func _process(delta):
 func _input(event):
 	if dragged_part != null:
 		dragged_part.set_global_pos(get_viewport().get_mouse_pos())
-		if event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_LEFT:
+		
+		var left = event.is_action_pressed("ui_left")
+		var right = event.is_action_pressed("ui_right")
+		var released_mouse = event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_LEFT
+		
+		if released_mouse:
 			dragged_part.connect_joints()
 			dragged_part = null
-		elif event.is_action_pressed("ui_left"):
-			var rot = _round_to(dragged_part.get_rotd(), 45)
-			dragged_part.set_rotd(rot)
-		elif event.is_action_pressed("ui_right"):
-			var rot = _round_to(dragged_part.get_rotd(), -45)
-			dragged_part.set_rotd(rot)
-
-func _round_to(val, div):
-	val = abs(int(val))
-	val -= val % abs((div))
-	return val + div
-	
+		elif left or right:
+			var num_45s = int(dragged_part.get_rotd()) / 45
+			num_45s += 1 if left else -1
+			dragged_part.set_rotd( (num_45s * 45) % 360 )
